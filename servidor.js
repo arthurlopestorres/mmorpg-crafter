c2502c20f5772796c231c554065f4ef8801a983b
Arthur Lopes Torres <arthurlopestorres@gmail.com>
2025-10-10 20:55:07 -0300

    atualizando porta do servidor

--- servidor.js
+++ servidor.js
@@ -3,7 +3,7 @@ const fs = require('fs').promises;
 const path = require('path');
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 const DATA_DIR = path.join(__dirname, 'data');
 const receitasFile = path.join(DATA_DIR, 'receitas.json');
 const componentesFile = path.join(DATA_DIR, 'componentes.json');
